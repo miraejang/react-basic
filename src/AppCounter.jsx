c7cc51101b2c5aac089f905377218d657db19675
miraejang <mirae611@nate.com>
2023-01-19 12:37:00 +0900

    basic

---
+++ src/AppCounter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+
+export default function Appcounter() {
+  const [total, setTotal] = useState(0);
+
+  const handleClick = () => setTotal((prev) => prev + 1);
+
+  return (
+    <div className="container">
+      <div className="banner">
+        Total Count: {total} {total > 10 ? "🔥" : "❄"}
+      </div>
+      <div className="counters">
+        <Counter total={total} onClick={handleClick} />
+        <Counter total={total} onClick={handleClick} />
+      </div>
+    </div>
+  );
+}
